2009d8ab5a346bf15761233dc0d9e2ab282ec2f7
mnuzzolo <michael.nuzzolo@gmail.com>
2025-03-04 20:17:20 -0600

    Adding comments.

--- Fetch_TakeHomeTests/ImageLoaderTests.swift
+++ Fetch_TakeHomeTests/ImageLoaderTests.swift
@@ -20,6 +20,7 @@ class ImageLoaderTests: XCTestCase {
 
     func test_givenImageLoader_verifyInitState() {
         let imageLoader = ImageLoader()
+        // Verifiy initial state
         XCTAssertEqual(imageLoader.state, ImageLoader.LoadingState.loading)
         XCTAssertNil(imageLoader.url)
     }
@@ -27,6 +28,7 @@ class ImageLoaderTests: XCTestCase {
     func test_givenImageLoaderWithInvalidUrl_verifyFailedState() async {
         let imageLoader = ImageLoader()
         let testURL = URL(string: "foo")!
+        // Verify that when attempting to load an invalid image, we hit the "failure" state
         await imageLoader.fetchImage(url: testURL)
         XCTAssertEqual(imageLoader.state, .failure)
     }
@@ -34,6 +36,7 @@ class ImageLoaderTests: XCTestCase {
     func test_givenImageLoaderWithValidUrl_verifySuccessState() async {
         let imageLoader = ImageLoader()
         let cacheManager = CacheManager.shared
+        // Verify that when we load a valid image, we reach the "success" state
         let testURL = URL(string: "https://d3jbb8n5wk0qxi.cloudfront.net/photos/535dfe4e-5d61-4db6-ba8f-7a27b1214f5d/small.jpg")!
         await imageLoader.fetchImage(url: testURL)
         let data = try! await cacheManager.load(url: testURL)
